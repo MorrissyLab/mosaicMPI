24dac5d507b510f7614c7ffecd682fad304a4e19
Ted Verhey <tbverhey@ucalgary.ca>
2023-08-16 10:55:12 -0600

    docstring update

--- src/cnmfsns/integration.py
+++ src/cnmfsns/integration.py
@@ -244,11 +244,11 @@ class Integration():
         """
         Select k-values for integration.
 
-        :param k_subset: k-values to use for integration. Either a Collection of integers, or a dict specifying k-values separately for each dataset. Defaults
-            to (2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60)
+        :param k_subset: k-values to use for integration. Either a Collection of integers, or a dict specifying k-values separately for each dataset.
+            Defaults to (2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60)
         :type k_subset: Union[Collection[int], Dict[str, Collection[int]]]
         :param exclude_unshared_k_values: in addition to the k_subset, also exclude k-values that are not shared with all datasets.
-        : type exclude_unshared_k_values: bool
+        :type exclude_unshared_k_values: bool, optional
 
         """
         for dataset_name in self.datasets:
